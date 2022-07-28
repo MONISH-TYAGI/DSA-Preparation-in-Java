32db0641f611835f85ac73ceb12560be99690e60
Monish <monishtyagi5770@gmail.com>
2022-07-28 20:56:17 +0530

    28july22

---
+++ 1. Pepcoding/LEVEL -1/4. Basic Data Structures/8. Graph (Archit Bhai)/21. Topological Sort( kahn's Algo)(BFS).java
@@ -0,0 +1,45 @@
+//https://practice.geeksforgeeks.org/problems/topological-sort/1
+class Solution
+{
+     static int[] BFS(int n, ArrayList<ArrayList<Integer>> adj){
+        int[] incoming = new int[n];
+        for(int src=0; src<adj.size(); src++){
+            for(Integer nbr: adj.get(src)){
+                incoming[nbr]++;
+            }
+        }
+        
+        Queue<Integer> q = new LinkedList<>();
+        // Multisource Breadth First Traversal
+        for(int i=0; i<n; i++){
+            if(incoming[i] == 0){
+                q.add(i);
+            }
+        }
+        
+        
+        int[] order = new int[n];
+        int idx = 0;
+        
+        while(q.size() > 0){
+            int src = q.remove();
+            order[idx++] = src;
+            
+            for(Integer nbr: adj.get(src)){
+                incoming[nbr]--;
+                if(incoming[nbr] == 0){
+                    q.add(nbr);
+                }
+            }
+        }
+        
+        return order;
+    }
+    static int[] topo;
+    static int idx;
+    //Function to return list containing vertices in Topological order. 
+    static int[] topoSort(int V, ArrayList<ArrayList<Integer>> adj) 
+    {
+return BFS(V,adj);
+    }
+}
---
+++ 1. Pepcoding/LEVEL -1/4. Basic Data Structures/8. Graph (Archit Bhai)/25.Two CLique Problem.java
@@ -0,0 +1,3 @@
+//https://www.codingninjas.com/codestudio/problems/two-cliques_1214524
+
+//CODE -> HW
